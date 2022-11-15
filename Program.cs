a09cf235a56de5f0b177e0ad68868ba0000350a4
Brainybrian316 <brainybrian316@gmail.com>
2022-11-14 22:14:03 -0500

    random badges with api generate successfully

--- Program.cs
+++ Program.cs
@@ -1,49 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace CatWorx.BadgeMaker
-{
-  class Program
-  {
-    static List<Employee> GetEmployees()
-    {
-      List<Employee> employees = new List<Employee>();
-      while (true)
-      {
-        Console.WriteLine("Please enter a first name: (leave empty to exit): ");
-        string firstName = Console.ReadLine() ?? "";
-        if (firstName == "")
-        {
-          break;
-        }
-        Console.Write("Enter last name: ");
-        string lastName = Console.ReadLine() ?? "";
-        Console.Write("Enter ID: ");
-        int id = Int32.Parse(Console.ReadLine() ?? "");
-        Console.Write("Enter Phot URL: ");
-        string photoUrl = Console.ReadLine() ?? "";
-        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-        employees.Add(currentEmployee);
-      }
-      return employees;
-    }
+namespace CatWorx.BadgeMaker {
+  class Program {
+   
 
-    static void PrintEmployees(List<Employee> employees)
-    {
-      for (int i = 0; i < employees.Count; i++)
-      {
+    static void PrintEmployees(List<Employee> employees){
+      for (int i = 0; i < employees.Count; i++){
         string template = "{0,  -10}\t{1, -20}\t{2}";
         Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
       }
     }
 
-  async static Task Main(string[] args) {
-  List<Employee> employees = GetEmployees();
+  async static Task Main(string[] args) {     
+    // List<Employee> employees = GetEmployees();
+    List<Employee> employees = await PeopleFetcher.GetFromApi();
+
     Util.PrintEmployees(employees);
     Util.MakeCSV(employees);
     await Util.MakeBadges(employees);
+
     }
   }
 }
