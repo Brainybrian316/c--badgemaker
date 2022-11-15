461c37d4ad19dd19dced2b692b8855330ac3b78c
Brainybrian316 <brainybrian316@gmail.com>
2022-11-14 22:31:54 -0500

    formatting

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -1,5 +1,4 @@
 
-using System.Security.AccessControl;
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
@@ -8,48 +7,54 @@ using System.Net.Http;
 
 
 
-namespace CatWorx.BadgeMaker {
-    class PeopleFetcher {
-          public static List<Employee> GetEmployees() {
-      List<Employee> employees = new List<Employee>();
-      while (true)  {
-        Console.WriteLine("Please enter a first name: (leave empty to exit): ");
-        string firstName = Console.ReadLine() ?? "";
-        if (firstName == "") {
-          break;
-        }
-        Console.Write("Enter last name: ");
-        string lastName = Console.ReadLine() ?? "";
-        Console.Write("Enter ID: ");
-        int id = Int32.Parse(Console.ReadLine() ?? "");
-        Console.Write("Enter Phot URL: ");
-        string photoUrl = Console.ReadLine() ?? "";
-        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-        employees.Add(currentEmployee);
-      }
-      return employees;
-    }
-
-async public static Task<List<Employee>> GetFromApi()
+namespace CatWorx.BadgeMaker
+{
+    class PeopleFetcher
     {
-      List<Employee> employees = new List<Employee>();
-        using (HttpClient client = new HttpClient())
-      {
-        string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
-        JObject json = JObject.Parse(response);
-       foreach (JToken token in json.SelectToken("results")!) {
-  // Parse JSON data
-  Employee emp = new Employee
-  (
-    token.SelectToken("name.first")!.ToString(),
-    token.SelectToken("name.last")!.ToString(),
-    Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
-    token.SelectToken("picture.large")!.ToString()
-  );
-  employees.Add(emp);
-}
-      }
-      return employees;
-    }
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a first name: (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Phot URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        async public static Task<List<Employee>> GetFromApi()
+        {
+            List<Employee> employees = new List<Employee>();
+            using (HttpClient client = new HttpClient())
+            {
+                string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                JObject json = JObject.Parse(response);
+                foreach (JToken token in json.SelectToken("results")!)
+                {
+                    // Parse JSON data
+                    Employee emp = new Employee
+                    (
+                      token.SelectToken("name.first")!.ToString(),
+                      token.SelectToken("name.last")!.ToString(),
+                      Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
+                      token.SelectToken("picture.large")!.ToString()
+                    );
+                    employees.Add(emp);
+                }
+            }
+            return employees;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace CatWorx.BadgeMaker {
-  class Program {
-   
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
 
-    static void PrintEmployees(List<Employee> employees){
-      for (int i = 0; i < employees.Count; i++){
-        string template = "{0,  -10}\t{1, -20}\t{2}";
-        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-      }
+
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,  -10}\t{1, -20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        async static Task Main(string[] args)
+        {
+            // List<Employee> employees = GetEmployees();
+            List<Employee> employees = await PeopleFetcher.GetFromApi();
+
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
+            await Util.MakeBadges(employees);
+
+        }
     }
-
-  async static Task Main(string[] args) {     
-    // List<Employee> employees = GetEmployees();
-    List<Employee> employees = await PeopleFetcher.GetFromApi();
-
-    Util.PrintEmployees(employees);
-    Util.MakeCSV(employees);
-    await Util.MakeBadges(employees);
-
-    }
-  }
 }
